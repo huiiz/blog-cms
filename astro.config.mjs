29a93a0cec50e198ffd65d7e75610faa5f8d9ef8
huiiz <1390113493@qq.com>
2026-04-25 00:53:29 +0800

    Update domain to hhuii-blog.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://blog.huii.top',
+  site: 'https://hhuii-blog.github.io',
   integrations: [
     mdx(),
     tailwind(),
